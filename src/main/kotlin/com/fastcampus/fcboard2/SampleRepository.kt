2139bd914f3922f465938f5f681d0020f4809749
harris.yi <harris.yi@kakaostyle.com>
2023-07-04 00:27:18 +0900

    feat: Add config

--- src/main/kotlin/com/fastcampus/fcboard2/SampleRepository.kt
+++ src/main/kotlin/com/fastcampus/fcboard2/SampleRepository.kt
@@ -2,5 +2,4 @@ package com.fastcampus.fcboard2
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface SampleRepository : JpaRepository<Sample, Long> {
-}
+interface SampleRepository : JpaRepository<Sample, Long>
